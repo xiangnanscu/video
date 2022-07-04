7c9f0fe2f29d86ed796ebad23f5fb6fb6232a75b
xiangnan <280145668@qq.com>
2022-07-04 16:08:27 +0000

    use script setup

--- src/App.vue
+++ src/App.vue
@@ -10,43 +10,33 @@
   <p>{{ message }}</p>
 </template>
 
-<script>
+<script setup>
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
-import { defineComponent, ref } from "vue";
+import { ref } from "vue";
 
-export default defineComponent({
-  name: "App",
-  setup() {
-    // app state
-    const ffmpeg = createFFmpeg({
-      log: true,
-    });
-    const message = ref("Click Start to Transcode");
-    let video = ref(null);
-    const file =
-      process.env.NODE_ENV === "production"
-        ? "/vue-app/flame.avi"
-        : "/flame.avi";
-    // methods
-    async function transcode() {
-      message.value = "Loading ffmeg-core.js";
-      await ffmpeg.load();
-      message.value = "Start transcoding";
-      ffmpeg.FS("writeFile", "test.avi", await fetchFile(file));
-      await ffmpeg.run("-i", "test.avi", "test.mp4");
-      message.value = "Complete transcoding";
-      const data = ffmpeg.FS("readFile", "test.mp4");
-      video.value = URL.createObjectURL(
-        new Blob([data.buffer], { type: "video/mp4" })
-      );
-    }
-    return {
-      video,
-      message,
-      transcode,
-    };
-  },
+// app state
+const ffmpeg = createFFmpeg({
+  log: true,
 });
+const message = ref("Click Start to Transcode");
+let video = ref(null);
+const file =
+  process.env.NODE_ENV === "production"
+    ? "/vue-app/flame.avi"
+    : "/flame.avi";
+// methods
+async function transcode() {
+  message.value = "Loading ffmeg-core.js";
+  await ffmpeg.load();
+  message.value = "Start transcoding";
+  ffmpeg.FS("writeFile", "test.avi", await fetchFile(file));
+  await ffmpeg.run("-i", "test.avi", "test.mp4");
+  message.value = "Complete transcoding";
+  const data = ffmpeg.FS("readFile", "test.mp4");
+  video.value = URL.createObjectURL(
+    new Blob([data.buffer], { type: "video/mp4" })
+  );
+}
 </script>
 
 <style>
